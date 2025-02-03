0fc68aad5ae5a1aa7010ba10d17aaeff9d12c86d
alex-sumy-ua <plachkovskyy@gmail.com>
2025-02-03 20:28:56 +0000

    initial commit

---
+++ src/test/java/com/gamboom/eventium/EventiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamboom.eventium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
