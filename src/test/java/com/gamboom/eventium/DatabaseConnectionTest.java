7cb10beafda029b630760e3b3450582eafc88224
alex-sumy-ua <plachkovskyy@gmail.com>
2025-02-04 20:51:30 +0000

    Database connection + Test

---
+++ src/test/java/com/gamboom/eventium/DatabaseConnectionTest.java
@@ -0,0 +1,30 @@
+package com.gamboom.eventium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class DatabaseConnectionTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void testDatabaseConnection() {
+        try (Connection connection = dataSource.getConnection()) {
+            assertNotNull(connection);
+            System.out.println("Connection successful: " + connection.getCatalog());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database connection failed.");
+        }
+    }
+
+}
