98b441691c4ef4b238b29fc0513c9feb2eeaa570
alex-sumy-ua <plachkovskyy@gmail.com>
2025-02-24 20:09:59 +0000

    fixed versions conflict between SpringBoot(3.2.10) and OpenAPI(2.5.0): v.3.4.3 -> 3.2.10

--- src/main/java/com/gamboom/eventium/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gamboom/eventium/exception/GlobalExceptionHandler.java
@@ -5,15 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Map<String, String>> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
+    public ResponseEntity<Map<String, String>> handleDataIntegrityViolationException(DataIntegrityViolationException exception, WebRequest request) {
+        // Exclude Swagger UI and OpenAPI endpoints
+        String path = request.getDescription(false);
+        if (path.contains("/v3/api-docs") || path.contains("/swagger-ui")) {
+            return null; // Let SpringDoc handle these requests
+        }
+
         Map<String, String> errorResponse = new HashMap<>();
 
         String errorMessage = exception.getMessage();
@@ -23,7 +30,7 @@ public class GlobalExceptionHandler {
         } else if (errorMessage.contains("unique_registration")) {
             errorResponse.put("message", "The user is already registered for this event");
         } else {
-            errorResponse.put("message", "A data integrity occurred" );
+            errorResponse.put("message", "A data integrity violation occurred");
         }
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
